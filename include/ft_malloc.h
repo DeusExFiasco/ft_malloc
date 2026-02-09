82df17e878677005cc56f641699815212338adf9
kixikCodes <noelmonzongarcia@gmail.com>
2026-02-09 13:21:49 +0100

    basic malloc and free implementation

--- include/ft_malloc.h
+++ include/ft_malloc.h
@@ -1,12 +1,14 @@
+#include <sys/mman.h>
 #include "../libft/include/libft.h"
 
-typedef int32_t t_word;
+#define MINALLOC 1024
 
+typedef int32_t t_word;
 typedef struct s_header t_header;
 typedef struct s_block t_block;
 
 struct s_header {
-    size_t size;
+    t_word size;
     t_block *next; 
 };
 
@@ -15,13 +17,6 @@ struct s_block {
     // memory
 };
 
-typedef struct s_heap {
-    t_block *freep;
-    int block_count;
-}   t_heap;
-
-const int word_size = sizeof(t_header);
-
 void *ft_malloc(size_t bytes);
-void *ft_realloc(size_t bytes, void *address);
-void ft_free(void *address);
+void *ft_realloc(size_t bytes, void *ptr);
+void ft_free(void *ptr);
--- src/ft_malloc.c
+++ src/ft_malloc.c
@@ -1,19 +1,87 @@
 #include "ft_malloc.h"
 
+static t_block base;
+static t_block *freep;
+
 static void init_heap() {
-    t_block dummy;
-    dummy.header.size = 0;
-    dummy.header.next = &dummy;
+    base.header.size = 0;
+    base.header.next = &base;
+    freep = &base;
+}
+
+static t_word btow(size_t bytes) {
+    return (bytes + sizeof(t_header) - 1 / sizeof(t_header) - 1);
+}
+
+static t_block *getmem(t_word words) {
+    if (words < MINALLOC)
+        words = MINALLOC;
+    size_t size = words * sizeof(t_header);
+    t_block *block = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+    if (block == MAP_FAILED)
+        return NULL;
+    block->header.size = words;
+    ft_free((void *)(block + 1));
+    return freep;
 }
 
 void *ft_malloc(size_t bytes) {
-    
+    t_block *prev;
+    t_block *curr;
+    t_word words;
+
+    if (freep == NULL)
+        init_heap();
+    words = bytes_to_units(bytes);
+    prev = freep;
+    curr = prev->header.next;
+
+    for (;; prev = curr, curr = curr->header.next) {
+        if (curr->header.size >= words) {
+            if (curr->header.size == words)
+                prev->header.next = curr->header.next;
+            else {
+                curr->header.size -= words;
+                curr += curr->header.size;
+                curr->header.size = words;
+            }
+            freep = prev;
+            return (void *)(curr + 1);
+        }
+        if (curr == freep) {
+            curr = getmem(words);
+            if (curr == NULL)
+                return NULL;
+        }
+    }
 }
 
-void *ft_realloc(size_t bytes, void *address) {
+void ft_free(void *ptr) {
+    t_block *block;
+    t_block *curr;
 
+    if (ptr == NULL)
+        return;
+    block = (t_block *)ptr - 1;
+
+    for (curr = freep; !(block > curr && block < curr->header.next); curr = curr->header.next) {
+        if (curr >= curr->header.next && (block > curr || block < curr->header.next))
+            break;
+    }
+    if (block + block->header.size == curr->header.next) {
+        block->header.size += curr->header.next->header.size;
+        block->header.next = curr->header.next->header.next;
+    } else
+        block->header.next = curr->header.next;
+    if (curr + curr->header.size == block) {
+        curr->header.size += block->header.size;
+        curr->header.next = block->header.next;
+    } else
+        curr->header.next = block;
+
+    freep = curr;
 }
 
-void ft_free(void *address) {
-
+void *ft_realloc(size_t bytes, void *ptr) {
+    // TODO: Implement lmao
 }
